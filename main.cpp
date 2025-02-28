3332ef39714ed83026098c445c2dd8831984adb3
ISRAEL <l24120356@morelia.tecnm.mx>
2025-02-27 22:35:30 -0600

    unidad3 ejemplo

--- main.cpp
+++ main.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 
-#include "recursividad_indirecta/Factorial.h"
-#include "recursividad_indirecta/Ulam.h"
 using namespace std;
-
+void cambio(int* a, int* b){
+    int temp = *a;
+    *a=*b;
+    *b = temp;
+}
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-    Ulam ulam;
-    int n;
-    cout << "Dame un numero: ";
-    cin>> n;
-    ulam.ulam(n);
-    return 0;
+    int variable = 10;
+    int* p;
+    int* q;
+    p = &variable;
+    q=p;
+    cout << "el valor de la variable: " << variable << endl;
+    cout<< " E l espacio en memoria es: " << *q<< endl;
+
+    int x=10, y=9;
+    cout << "x: " << x << " y: " << y << endl;
+    cambio(&x,&y);
+    cout << "x: " << x << " y: " << y << endl;
 }
 
 // TIP See CLion help at <a
