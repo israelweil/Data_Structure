215f8b816e516a02aa3ae2777cc5f8ddf8bbff21
ISRAEL <l24120356@morelia.tecnm.mx>
2025-05-01 00:04:20 -0600

    falta terminar

---
+++ src/Ejercicio.cpp
@@ -0,0 +1,5 @@
+//
+// Created by cland on 30/04/2025.
+//
+
+#include "Ejercicio.h"
---
+++ src/Ejercicio.h
@@ -0,0 +1,19 @@
+//
+// Created by cland on 30/04/2025.
+//
+
+#ifndef EJERCICIO_H
+#define EJERCICIO_H
+struct Nodo {
+    int valor;
+    Nodo* siguiente;
+};
+
+
+class Ejercicio {
+
+};
+
+
+
+#endif //EJERCICIO_H
